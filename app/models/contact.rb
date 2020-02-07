3eb3f5be8e1d95f7e8ab41921fdc6c3e461c88ca
Nick Barry <itsnickbarry@protonmail.ch>
2020-02-07 17:32:26 -0600

    fix fixtures, test contact associations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,26 @@
 class Contact < ApplicationRecord
+  validate :has_at_least_one_email
+  validate :has_at_least_one_phone_number
+
   has_many :addresses
   has_many :emails
   has_many :phone_numbers
+
+  accepts_nested_attributes_for :addresses, allow_destroy: true
+  accepts_nested_attributes_for :emails, allow_destroy: true
+  accepts_nested_attributes_for :phone_numbers, allow_destroy: true
+
+   private
+
+    def has_at_least_one_email
+      if emails.empty?
+        errors.add(:emails, 'must have at least one')
+      end
+    end
+
+    def has_at_least_one_phone_number
+      if phone_numbers.empty?
+        errors.add(:phone_numbers, 'must have at least one')
+      end
+    end
 end
--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class ContactTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  before do
+    @contact = Contact.create({
+      first_name: 'fn', last_name: 'ln',
+      addresses_attributes: [{ body: 'address' }],
+      emails_attributes: [{ body: 'email' }],
+      phone_numbers_attributes: [{ body: 'phone_number' }],
+    })
+  end
+
+  describe Contact do
+    it 'is valid' do
+      assert @contact.valid?
+    end
+
+    it 'does not require addresses' do
+      @contact.addresses.delete_all
+      assert @contact.valid?
+    end
+
+    it 'must have at least one email' do
+      @contact.emails.delete_all
+      refute @contact.valid?
+    end
+
+    it 'must have at least one phone number' do
+      @contact.phone_numbers.delete_all
+      refute @contact.valid?
+    end
+  end
 end
