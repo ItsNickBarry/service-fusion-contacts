f81dea680c05927d44a1bbc0b5e629c91cc490c0
Nick Barry <itsnickbarry@protonmail.ch>
2020-02-10 20:25:19 -0600

    validate phone number format, use faker for phone number and email seeds

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,4 +1,8 @@
 class PhoneNumber < ApplicationRecord
-  validates :body, presence: true
+  validates :body, presence: true, phone: { possible: true }
   belongs_to :contact
+
+  def body= body
+    super(Phonelib.parse(body).international)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,75 +7,80 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 def phone_number
-  "+" + (0..(1 + rand(3))).map { rand 1000 }.join(' ')
+  # generate valid phone number according to Phonelib
+  number = nil
+  until Phonelib.parse(number).possible?
+    number = Faker::PhoneNumber.phone_number_with_country_code
+  end
+  number
 end
 
 Contact.create!({
   first_name: 'Gladwin', last_name: 'Jebb', date_of_birth: Date.new(1900, 4, 29),
   addresses_attributes: [],
-  emails_attributes: [{ body: 'Gladwin@example.com' }],
+  emails_attributes: [{ body: Faker::Internet.email }],
   phone_numbers_attributes: [{ body: phone_number }, { body: phone_number }],
 })
 
 Contact.create!({
   first_name: 'Trygve', last_name: 'Lie', date_of_birth: Date.new(1896, 7, 16),
   addresses_attributes: [],
-  emails_attributes: [{ body: 'Trygve@example.com' }],
+  emails_attributes: [{ body: Faker::Internet.email }],
   phone_numbers_attributes: [{ body: phone_number }, { body: phone_number }],
 })
 
 Contact.create!({
   first_name: 'Dag', last_name: 'Hammarskjöld', date_of_birth: Date.new(1905, 7, 29),
   addresses_attributes: [],
-  emails_attributes: [{ body: 'Dag@example.com' }],
+  emails_attributes: [{ body: Faker::Internet.email }],
   phone_numbers_attributes: [{ body: phone_number }, { body: phone_number }],
 })
 
 Contact.create!({
   first_name: 'U', last_name: 'Thant',
   addresses_attributes: [{ body: 'Burma' }, { body: 'Myanmar' }], date_of_birth: Date.new(1909, 1, 22),
-  emails_attributes: [{ body: 'U@example.com' }],
+  emails_attributes: [{ body: Faker::Internet.email }],
   phone_numbers_attributes: [{ body: phone_number }, { body: phone_number }],
 })
 
 Contact.create!({
   first_name: 'Kurt', last_name: 'Waldheim', date_of_birth: Date.new(1918, 12, 21),
   addresses_attributes: [],
-  emails_attributes: [{ body: 'Kurt@example.com' }],
+  emails_attributes: [{ body: Faker::Internet.email }],
   phone_numbers_attributes: [{ body: phone_number }, { body: phone_number }],
 })
 
 Contact.create!({
   first_name: 'Javier', last_name: 'Pérez de Cuéllar', date_of_birth: Date.new(1920, 1, 19),
   addresses_attributes: [{ body: 'Peru' }],
-  emails_attributes: [{ body: 'Javier@example.com' }],
+  emails_attributes: [{ body: Faker::Internet.email }],
   phone_numbers_attributes: [{ body: phone_number }, { body: phone_number }],
 })
 
 Contact.create!({
   first_name: 'Boutros', last_name: 'Boutros-Ghali', date_of_birth: Date.new(1922, 11, 14),
   addresses_attributes: [],
-  emails_attributes: [{ body: 'Boutros@example.com' }],
+  emails_attributes: [{ body: Faker::Internet.email }],
   phone_numbers_attributes: [{ body: phone_number }, { body: phone_number }],
 })
 
 Contact.create!({
   first_name: 'Kofi', last_name: 'Annan', date_of_birth: Date.new(1938, 4, 8),
   addresses_attributes: [],
-  emails_attributes: [{ body: 'Kofi@example.com' }],
+  emails_attributes: [{ body: Faker::Internet.email }],
   phone_numbers_attributes: [{ body: phone_number }, { body: phone_number }],
 })
 
 Contact.create!({
   first_name: 'Ban', last_name: 'Ki-moon', date_of_birth: Date.new(1944, 6, 13),
   addresses_attributes: [],
-  emails_attributes: [{ body: 'Ban@example.com' }],
+  emails_attributes: [{ body: Faker::Internet.email }],
   phone_numbers_attributes: [{ body: phone_number }, { body: phone_number }],
 })
 
 Contact.create!({
   first_name: 'António', last_name: 'Guterres', date_of_birth: Date.new(1949, 4, 30),
   addresses_attributes: [],
-  emails_attributes: [{ body: 'António@example.com' }],
+  emails_attributes: [{ body: Faker::Internet.email }],
   phone_numbers_attributes: [{ body: phone_number }, { body: phone_number }],
 })
